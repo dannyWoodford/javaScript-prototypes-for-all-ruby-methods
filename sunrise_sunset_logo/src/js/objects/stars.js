7bbce19786832c331935718e266d47da393247be
Danny Woodford <Daniel.woodford.g@gmail.com>
2020-04-14 23:45:37 -0400

    scene finish, now to add off canvas content

---
+++ sunrise_sunset_logo/src/js/objects/stars.js
@@ -0,0 +1,87 @@
+function addStars(quantity, size, color){
+
+    const geometry = new THREE.BufferGeometry();
+    const vertices = [];
+
+    for (let i = 0; i < quantity; i++) {
+        vertices.push( 
+            Math.random() * 350 - 200,
+            Math.random() * 80 - 40,
+            Math.random() * -150 - 40
+        );
+    }
+        
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    const material =  new THREE.PointsMaterial({
+        color: color,
+        size: size,
+        transparent: true,
+        map: THREE.ImageUtils.loadTexture(
+            'assets/images/snow_mask_2.png'),
+        blending: THREE.AdditiveBlending,
+    })
+
+    stars = new THREE.Points( geometry, material );
+        stars.rotation.x = -1.77
+        stars.rotation.y = -3.15
+            stars.material.opacity = Math.random()* .3 - .3
+    scene.add(stars);
+}
+
+
+
+
+
+
+function addFlickerStars(quantity, size, color){
+
+    const geometry = new THREE.BufferGeometry();
+    const vertices = [];
+
+    for (let i = 0; i < quantity; i++) {
+        vertices.push( 
+            Math.random() * 350 - 200,
+            Math.random() * 80 - 40,
+            Math.random() * -150 - 40
+        );
+    }
+        
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    const material =  new THREE.PointsMaterial({
+        color: color,
+        size: size,
+        transparent: true,
+        map: THREE.ImageUtils.loadTexture(
+            'assets/images/snow_mask_2.png'),
+        blending: THREE.AdditiveBlending,
+    })
+
+    flickerStars = new THREE.Points( geometry, material );
+        flickerStars.rotation.x = -1.77
+        flickerStars.rotation.y = -3.15
+            flickerStars.material.opacity = Math.random()* .3 - .3
+    scene.add(flickerStars);
+}
+
+function addSpace(){
+    let loader = new THREE.TextureLoader();
+    loader.load('./assets/images/night_sky_bg3.png', function(texture){
+
+        geometry = new THREE.PlaneBufferGeometry(1500,1000);
+        material = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true,
+        })
+        material.depthWrite = false;
+
+        nightSky = new THREE.Mesh(geometry, material)
+        nightSky.position.set(-170,100,-200)
+        nightSky.rotation.x = -1.80
+        nightSky.rotation.y = -3.15
+        nightSky.material.opacity = .3
+
+        scene.add(nightSky) 
+    })
+}
